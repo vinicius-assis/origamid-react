86182ba550e53b1410b57c093bdf836c84476b32
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-15 11:44:08 -0300

    fix routes and add styles

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
 import LoginCreate from './LoginCreate';
 import LoginForm from './LoginForm';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 
-const Login = () => (
-  <div>
-    <Routes>
-      <Route path="/" element={<LoginForm />} />
-      <Route path="criar" element={<LoginCreate />} />
-      <Route path="perdeu" element={<LoginPasswordLost />} />
-      <Route path="resetar" element={<LoginPasswordReset />} />
-    </Routes>
-  </div>
-)
+import styles from './Login.module.css'
+
+const Login = () => {
+  const { login } = useContext(UserContext)
+
+  if (login === true ) return <Navigate to="/conta" />
+  return (
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
+  )
+}
 
 export default Login;
