23c3898163a608ff53908421f47a621427f6a055
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-15 09:44:11 -0300

    create functions to api

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { TOKEN_POST, USER_GET } from '../../api';
 import useForm from '../../Hooks/useForm';
 import ButtonForm from '../ButtonForm';
 import InputForm from '../InputForm';
 
 
 const LoginForm = () => {
-  const username = useForm('email')
+  const username = useForm()
   const password = useForm()
+
+  useEffect(() => {
+    const token = window.localStorage.getItem('token')
+    if (token) {
+      getUser(token)
+    }
+  })
+
+  async function getUser(token) {
+    const {url, options} = USER_GET(token)
+    const response = await fetch(url, options)
+    const json = await response.json()
+    console.log(json)
+  }
   
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify()
-    }).then((response) => {
-      console.log(response)
-      return response.json()
-    }).then((json) => {
-      console.log(json)
-    })
+  async function handleSubmit(event) {
+    event.preventDefault()
+
+    if (username.validate() && password.validate()) {
+      const {url, options} = TOKEN_POST({username: username.value, password: password.value})
+
+      const response = await fetch(url, options)
+      const json = await response.json()
+
+      window.localStorage.setItem('token', json.token)
+      getUser(json.token)
+    }
+
   }
 
   return (
