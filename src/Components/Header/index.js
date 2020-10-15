b20645645f70a4dd6747fcecbdf6122ec3fdb71e
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-15 11:41:29 -0300

    Add state to logout

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css'
 import {ReactComponent as Dogs} from '../../assets/dogs.svg'
+import { UserContext } from '../../UserContext';
 
 
-const Header = () => (
-  <header className={styles.header}>
-    <nav className={`${styles.nav} container`}>
-      <Link className={styles.logo} to="/" aria-label="Dogs - Home">
-        <Dogs />
-      </Link>
-      <Link className={styles.login} to="/login">Login | Criar</Link>
-    </nav>
-  </header>
-)
-
+const Header = () => {
+  const { userLogout, data } = useContext(UserContext)
+  
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login | Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  )
+}
 export default Header;
