ac4de2886551bb4650ee2a62fbc6e148005db3f5
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-16 14:47:54 -0300

    Add footer component

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from './Footer.module.css'
+import { ReactComponent as Dogs} from '../../assets/dogs-footer.svg'
 
 
-const Footer = () => <div className={styles.footer}></div>
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  )
+}
 
 export default Footer;
