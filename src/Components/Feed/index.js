98d419709071472fba8cfe453a0ed7df0e724c07
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-16 09:00:34 -0300

    Create Feed component

---
+++ src/Components/Feed/index.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import FeedModal from './FeedModal';
+import FeedPhotos from './FeedPhotos';
+
+
+function Feed() {
+  const [modalPhoto, setModalPhoto] = useState(null)
+
+
+
+  return (
+    <div>
+      {modalPhoto && <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto}/>}
+      <FeedPhotos setModalPhoto={setModalPhoto} />
+    </div>
+  );
+}
+
+export default Feed;
