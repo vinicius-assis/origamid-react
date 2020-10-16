cc328c1328dc98d229e82921ccc6ea409b867c0e
Vinicius <viniciusassis.dev@hotmail.com>
2020-10-16 08:57:54 -0300

    Create FeedPhotos component

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import FeedPhotosItem from './FeedPhotosItem';
+import Error from '../Helpers/Error';
+import Loading from '../Helpers/Loading';
+import useFetch from '../../Hooks/useFetch'
+import { PHOTOS_GET } from '../../api';
+
+import styles from './FeedPhotos.module.css'
+
+const FeedPhotos = ({ setModalPhoto }) => {
+  const { data, loading, error, request } = useFetch()
+
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { url, options } = PHOTOS_GET({page: 1, total: 6, user: 0})
+      const { json } = await request(url, options)
+      console.log(json)
+    }
+
+    fetchPhotos()
+  }, [request])
+  
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if(data)
+  return (
+    <ul className={`${styles.feed} animeLeft`}>
+      {data.map(photo => 
+        <FeedPhotosItem 
+          key={photo.id} 
+          photo={photo} 
+          setModalPhoto={setModalPhoto} 
+        />)
+      }
+    </ul>
+  )
+  else return null
+}
+
+export default FeedPhotos;
